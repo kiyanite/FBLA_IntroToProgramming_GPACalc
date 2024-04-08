9a7baf6ff5636b8a7675cfa6563747cc333ca994
Reva Karri <123896586+kiyanite@users.noreply.github.com>
2024-04-07 18:27:21 -0700

    rev
    messing around with the routing, trying to solve bug

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,16 +1,41 @@
+// import { NgModule } from "@angular/core";
+// import { RouterModule, Routes } from "@angular/router";
+// // import { AppComponent } from "./app/app.component";
+// import { GpaCalcComponent } from "./app/gpa-calc/gpa-calc.component";
+// import { LoginComponent } from "./app/login/login.component";
+// import { LoginErrorComponent } from "./app/login-error/login-error.component";
+// import { GpaAutoComponent } from "./app/gpa-auto/gpa-auto.component";
+
+
+//  const routes: Routes = [
+// //     { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
+// //     { path: 'gpa-calc', component: GpaCalcComponent},
+// //     { path: 'google-classroom/login', component: LoginComponent},
+//     { path: 'login', component: LoginComponent },
+//     { path: 'gpa-auto', component: GpaAutoComponent },
+//     { path: 'login-error', component: LoginErrorComponent },
+//  ];
+
+// @NgModule({
+//     imports: [RouterModule.forRoot(routes)],
+//     exports: [RouterModule]
+// })
+
+// export class AppRoutingModule { }
+
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 // import { AppComponent } from "./app/app.component";
-import { GpaCalcComponent } from "./app/gpa-calc/gpa-calc.component";
 import { LoginComponent } from "./app/login/login.component";
-import { LoginErrorComponent } from "./app/login-error/login-error.component";
+import { GpaCalcComponent } from "./app/gpa-calc/gpa-calc.component";
 import { GpaAutoComponent } from "./app/gpa-auto/gpa-auto.component";
-
+import { LoginErrorComponent } from "./app/login-error/login-error.component";
 
  const routes: Routes = [
-//     { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
+//    { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
 //     { path: 'gpa-calc', component: GpaCalcComponent},
 //     { path: 'google-classroom/login', component: LoginComponent},
+    { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'gpa-auto', component: GpaAutoComponent },
     { path: 'login-error', component: LoginErrorComponent },
