d6eae793d1e40df7ef01b1a1b3f29021d697d009
Reva Karri <123896586+kiyanite@users.noreply.github.com>
2024-04-08 20:58:39 -0700

    rev
    apparently app-routing modules doesnt work, so moving routing to app-routing.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { GpaCalcComponent } from "./gpa-calc/gpa-calc.component";
 import { LoginComponent } from "./login/login.component";
+import { GpaAutoComponent } from "./gpa-auto/gpa-auto.component";
+import { LoginErrorComponent } from "./login-error/login-error.component";
 
 export const APP_ROUTES: Routes = [
+    // { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
+    // { path: 'gpa-calc', component: GpaCalcComponent},
+    // { path: 'google-classroom/login', component: LoginComponent},
     { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
-    { path: 'gpa-calc', component: GpaCalcComponent},
-    { path: 'google-classroom/login', component: LoginComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'gpa-auto', component: GpaAutoComponent },
+    { path: 'login-error', component: LoginErrorComponent },
 ];
