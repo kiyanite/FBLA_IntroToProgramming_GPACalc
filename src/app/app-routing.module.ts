a4bd41d85cf1db9caad1a7b89bdbe0249019e064
Reva Karri <123896586+kiyanite@users.noreply.github.com>
2024-04-08 19:36:11 -0700

    rev
    i think i got oAuth to work!!! lets see

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-// import { AppComponent } from "./app/app.component";
-import { LoginComponent } from "./login/login.component";
-import { GpaCalcComponent } from "./gpa-calc/gpa-calc.component";
-import { GpaAutoComponent } from "./gpa-auto/gpa-auto.component";
-import { LoginErrorComponent } from "./login-error/login-error.component";
+// import { NgModule } from "@angular/core";
+// import { RouterModule, Routes } from "@angular/router";
+// // import { AppComponent } from "./app/app.component";
+// import { LoginComponent } from "./login/login.component";
+// import { GpaCalcComponent } from "./gpa-calc/gpa-calc.component";
+// import { GpaAutoComponent } from "./gpa-auto/gpa-auto.component";
+// import { LoginErrorComponent } from "./login-error/login-error.component";
 
- const routes: Routes = [
-//    { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
-//     { path: 'gpa-calc', component: GpaCalcComponent},
-//     { path: 'google-classroom/login', component: LoginComponent},
-    { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'gpa-auto', component: GpaAutoComponent },
-    { path: 'login-error', component: LoginErrorComponent },
- ];
+//  const routes: Routes = [
+// //    { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
+// //     { path: 'gpa-calc', component: GpaCalcComponent},
+// //     { path: 'google-classroom/login', component: LoginComponent},
+//     { path: '', redirectTo: '/gpa-calc', pathMatch: 'full' },
+//     { path: 'login', component: LoginComponent },
+//     { path: 'gpa-auto', component: GpaAutoComponent },
+//     { path: 'login-error', component: LoginErrorComponent },
+//  ];
 
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-})
+// @NgModule({
+//     imports: [RouterModule.forRoot(routes)],
+//     exports: [RouterModule]
+// })
 
-export class AppRoutingModule { }
+// export class AppRoutingModule { }
